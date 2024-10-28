3f1e37f8173dba8bd9242f51fcaa17c27bfa9335
Francisco Membrive <130402424+francisconassif@users.noreply.github.com>
2024-10-28 00:12:46 -0300

    Create A.cpp

---
+++ Online Contests/Codeforces Global Round 27/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define dbg(x) cout << #x << " = " << x << endl
+#define FOR(i, a, n) for(ll i = a; i < n; i++)
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+signed main(){ _
+    ll n, m, r, c;
+    ll sum = 0;
+    int t; cin >> t;
+    while(t--){
+        cin >> n >> m >> r >> c;
+        sum = m-c + (n-r)*(2*m-1);
+        cout<<sum<<'\n';
+    }
+    
+    return 0;
+}
+
+
